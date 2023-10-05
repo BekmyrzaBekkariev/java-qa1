fc49b3f13b266296138b5bc096e89a4d9571b927
Bekmyrza <bekmyrzabekkariev05@gmail.com>
2023-10-05 18:09:36 +0600

    gradle

---
+++ two/square.java
@@ -0,0 +1,20 @@
+package two;
+
+public class square {
+	public static void main(String[] args) {
+
+	}
+	public double len;
+
+	public square(double len){
+		this.len = len;
+	}
+
+	public double area(){
+		return  len * len;
+	}
+
+
+
+
+}
